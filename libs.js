0764462d283a9b814a43f4499ab0378779c362f5
radoslaw <radoslaw.redzik@ggsitc.com>
2024-10-17 11:29:26 +0200

    Update libs.js

--- libs.js
+++ libs.js
@@ -8,8 +8,7 @@
  *
  * Date: 2023-08-28T13:37Z
  */
-"use strict";
-function(e, t) {
+!function(e, t) {
     "use strict";
     "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function(e) {
         if (!e.document)
@@ -18864,3 +18863,5 @@ function(e, t) {
     G
 }
 ));
+
+var $h = jQuery.noConflict();
